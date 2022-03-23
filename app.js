22fe27a9346cbc835bd1c421d55db01c91e28e07
asb <ajm153123@naver.com>
2022-03-23 15:45:45 +0900

    second pratice commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host : 'localhost',
     port: 3306,
     user : 'root',
-    password : '4612',
+    password : 'skhuA+4.5',
     database : 'sample1'
 })
 
--- test.js
+++ test.js
@@ -1,1 +1,48 @@
-console.log("test commit")
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+host : "localhost",
+port : 3306,
+user : "SWP",
+password : "testing00",
+database : "sample1"
+})
+
+connection.connect(()=>{
+    console.log("connection")
+})
+
+console.log("ing");
+app.get("/test", function (req, res) {
+    // connection.query('select * from user', function (err, rows) {
+    connection.query('show databases;', function (err, rows) {
+        if (err) {
+        console.log("err :"+err)
+        }
+
+        console.log('Open DataBase');
+        res.send("hello")
+        res.json(rows);
+    });
+});
+
+app.post('/post', (req, res) => {
+    console.log('who get in here post /users');
+    connection.query('select pc_status from pc ', function (err, rows) {
+        if (err) {
+        console.log("err :"+err)
+        }
+
+        console.log('Open DataBase');
+        res.json(rows);
+    });
+});
+
+app.listen(8888, () => {
+    console.log('Example app listening on port 3000!');
+});
